68a4130002b30f9943739163a7735ab6b51b0cca
sooriyabandaradeemantha <sooriyabandaradeemantha@gmail.com>
2026-01-01 21:57:09 +0530

    Add files via upload

---
+++ Lesson5.py
@@ -0,0 +1,6 @@
+number = int (input("Enter a number: "))
+print ("number entered by you is:", number)
+if number % 2 == 0:
+    print ("The number is even")    
+else:
+    print ("The number is odd")
