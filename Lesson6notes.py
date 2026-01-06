ae01cd878da55c9c16e4fa925440c51d9143b40c
sooriyabandaradeemantha <sooriyabandaradeemantha@gmail.com>
2026-01-06 21:59:41 +0530

    Add files via upload

---
+++ Lesson6notes.py
@@ -0,0 +1,20 @@
+#for loop
+name = 'Peter J'
+for c in name:
+    print(c)
+
+#while loop
+i = 1  #initialization
+while i<=10: #condition checking
+    print(i)
+    i = i + 1 #incrementation
+
+#nested loop
+fruits = ['Plant1','Plant2','Plant3']
+colors = ['W1','W2','W3']
+for f in fruits:
+    for c in colors:
+        print(c)
+    print(f)
+
+    
