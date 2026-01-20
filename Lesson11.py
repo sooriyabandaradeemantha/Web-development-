42136be55d597526ed9dfc7ce153a051f81e3757
The Orca God <sooriyabandaradeemantha@gmail.com>
2026-01-20 21:49:52 +0530

    Add files via upload

---
+++ Lesson11.py
@@ -0,0 +1,15 @@
+my_dict = {}
+my_dict1 = {1: 'apple', 2: 'ball'}
+my_dict2 = {'name': 'John', 1: [2, 4, 3]}
+my_dict3 = {'name': 'Jack', 'age': 25}
+print(my_dict3['name'])
+print(my_dict3.get('age'))
+my_dict3 ['age'] = 27
+print(my_dict3)
+my_dict3 ['address'] = 'Downtown'
+print(my_dict)
+my_dict3.pop('age')
+print(my_dict3)
+print("Address:", my_dict3.get('address'))
+my_dict3.clear()
+
