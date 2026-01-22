ad4725df7e1f47ed201b47314a57533fb6858242
The Orca God <sooriyabandaradeemantha@gmail.com>
2026-01-22 21:54:45 +0530

    Add files via upload

---
+++ Lesson13.py
@@ -0,0 +1,20 @@
+def fib_series(nterms):
+    n1, n2 = 0,1
+    count = 0
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
+nterms = int(input("How many terms?"))
+
+if nterms <= 0:
+    print("Please enter a positive integer")
+elif nterms == 1:
+    print("Fibonacci sequence upto", nterms, ":")
+    fib_series(nterms)
+else:
+    print("Fibonacci sequence:")
+    fib_series(nterms)
+
